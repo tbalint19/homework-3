6b27e3ce816f5647c935cdda838d20645e719a55
tbalint19 <toth910719balint@gmail.com>
2016-06-05 18:11:17 +0200

    Add files via upload

--- printing.py
+++ printing.py
@@ -1,9 +1,16 @@
 from reports import *
 
-print(count_games('game_stat.txt'))
-print(decide('game_stat.txt', 2009))
-print(get_latest('game_stat.txt'))
-print(count_by_genre('game_stat.txt', 'RPG'))
-print(get_line_number_by_title('game_stat.txt', 'Diablo II'))
+print("All games: " + str(count_games('game_stat.txt')))
+print("Is there a game?: " + str(decide('game_stat.txt', 2009)))
+print("Latest game: " + str(get_latest('game_stat.txt')))
+print("How many from a genre?: " + str(count_by_genre('game_stat.txt', 'RPG')))
+print("Which line: " + str(get_line_number_by_title('game_stat.txt', 'Diablo II')))
+print("Most played: " + str(get_most_played('game_stat.txt')))
+print("All sold: " + str(sum_sold('game_stat.txt')))
+print("Average of sells: " + str(get_selling_avg('game_stat.txt')))
+print("Longest name: " + str(count_longest_title('game_stat.txt')))
+print("Average of releases: " + str(get_date_avg('game_stat.txt')))
+print("All about a game:\n" + str(get_game('game_stat.txt', 'Diablo III')))
+print(count_grouped_by_genre('game_stat.txt'))
 
 # according to this test, it works fine
